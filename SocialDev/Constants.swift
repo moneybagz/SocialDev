ff3a2b3a3be5293eb37f0230b81db9b4a6430f34
Clyff Millet <clyffm@gmail.com>
2017-01-26 20:34:37 -0500

    Sign in ui custom classescomplete

---
+++ SocialDev/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialDev
+//
+//  Created by Clyfford Millet on 1/26/17.
+//  Copyright © 2017 Clyff Millet. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
