7b4c402aa58eca62e9267028520cd7352ed122bd
Clyff Millet <clyffm@gmail.com>
2017-01-29 15:19:39 -0500

    email authentication working

---
+++ Pods/Target Support Files/Pods-SocialDev/Pods-SocialDev-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SocialDev : NSObject
+@end
+@implementation PodsDummy_Pods_SocialDev
+@end
---
+++ SocialDev/SignInVC.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  SocialDev
+//
+//  Created by Clyfford Millet on 1/26/17.
+//  Copyright © 2017 Clyff Millet. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
+
+class SignInVC: UIViewController {
+    
+    
+    @IBOutlet var emailField: FancyField!
+    @IBOutlet var pwdField: FancyField!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func facebookBtnPressed(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("CLYFF: Unable to authenticate with facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("CLYFF: User cancelled authentication")
+            } else {
+                print("CLYFF: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("CLYFF: Unable to authenticate with Firebase -\(error)")
+            } else {
+                print("CLYFF: Successfully authenticated with Firebase")
+            }
+        })
+    }
+
+    @IBAction func signInTapped(_ sender: AnyObject) {
+        if let email = emailField.text, let pwd = pwdField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("CLYFF: Email user authenticated with firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("CLYFF: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("Clyff: Successfully authenticated with firebase")
+                        }
+                    })
+                }
+            })
+        }
+    }
+}
+
