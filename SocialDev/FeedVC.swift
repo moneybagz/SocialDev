fb4d813ab50ce8bc83df52b498265e3eb5ebe6c6
Clyff Millet <clyffm@gmail.com>
2017-02-05 16:19:09 -0500

    upload metadata

--- SocialDev/FeedVC.swift
+++ SocialDev/FeedVC.swift
@@ -14,10 +14,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet var imageAdd: CircleView!
+    @IBOutlet var captionField: FancyField!
     
     var posts = [Post]()
     var imagePicker: UIImagePickerController!
     static var imageCache: NSCache<NSString, UIImage> = NSCache()
+    var imageSelected: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -78,6 +80,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
         
         if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
             imageAdd.image = image
+            imageSelected = true
         } else {
             print("CLYFF: Invalid image wasn't selected")
         }
@@ -89,6 +92,54 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
         present(imagePicker, animated: true, completion: nil)
     }
 
+    @IBAction func postBtnTapped(_ sender: AnyObject) {
+        
+        guard let caption = captionField.text, caption != "" else {
+            print("CLYFF: Caption must be entered")
+            return
+        }
+        guard let img = imageAdd.image, imageSelected == true else {
+            print("CLYFF: An image must be selected")
+            return
+        }
+        
+        if let imgData = UIImageJPEGRepresentation(img, 0.2) {
+            
+            let imgUid = NSUUID().uuidString
+            let metadata = FIRStorageMetadata()
+            metadata.contentType = "image/jpeg"
+            
+            DataService.ds.REF_POST_IMAGES.child(imgUid).put(imgData, metadata: metadata, completion: { (metadata, error) in
+                if error != nil {
+                    print("CLYFF: Unable to upload image to firebase storage")
+                } else {
+                    print("CLYFF: Successfully uploaded image to firebase storage")
+                    let downloadURL = metadata?.downloadURL()?.absoluteString
+                    if let url = downloadURL {
+                        self.postToFirebase(imgURL: url)
+                    }
+                }
+            })
+        }
+    }
+    
+    func postToFirebase(imgURL: String) {
+        let post: Dictionary<String, Any> = [
+                                            "caption": captionField.text!,
+                                            "imageURL": imgURL,
+                                            "likes": 0
+                                            ]
+        
+        let firebasePost = DataService.ds.REF_POSTS.childByAutoId()
+        firebasePost.setValue(post)
+        
+        captionField.text = ""
+        imageSelected = false
+        imageAdd.image = UIImage(named: "add-image")
+        
+        tableView.reloadData()
+    }
+    
     @IBAction func signOutTapped(_ sender: AnyObject) {
         KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         try! FIRAuth.auth()?.signOut()
